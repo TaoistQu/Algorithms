9e716ad0ed23adb4f29a8d4fd50aea2fa9399867
TaoistQu <42109009+TaoistQu@users.noreply.github.com>
2020-07-27 00:28:13 +0800

    add:增加冒泡排序代码

---
+++ src/main/java/cn/algorithms/sort/BubbleSort.java
@@ -0,0 +1,59 @@
+package cn.algorithms.sort;
+
+import cn.algorithms.util.ArrayUtil;
+
+/**
+ * 测试冒泡排序时间复杂度 O(n^2)
+ *
+ * @author TaoistQu
+ * @data 2020-07-27 00:00
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxValue = 100;
+        int maxSize = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = ArrayUtil.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = ArrayUtil.copyArray(arr1);
+            bubbleSort(arr1);
+            ArrayUtil.comparator(arr2);
+            if (!ArrayUtil.isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice" : "Fucking fucked!");
+        int[] arr = ArrayUtil.generateRandomArray(maxSize, maxValue);
+        ArrayUtil.printArray(arr);
+        bubbleSort(arr);
+        ArrayUtil.printArray(arr);
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param arr
+     */
+    public static void bubbleSort(int arr[]) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j]  >arr[j+1]) {
+                    ArrayUtil.swap(arr, j, j + 1);
+                }
+            }
+        }
+       /* for (int e = arr.length - 1; e > 0; e--) {
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    ArrayUtil.swap(arr, i, i + 1);
+                }
+            }
+        }*/
+    }
+}
---
+++ src/main/java/cn/algorithms/util/ArrayUtil.java
@@ -0,0 +1,93 @@
+package cn.algorithms.util;
+
+import java.util.Arrays;
+
+/**
+ * @author TaoistQu
+ * @data 2020-07-27 00:10
+ */
+public class ArrayUtil {
+    /**
+     * 交换数组两位置上的值 (这个地方有个坑，必须要两个数不相等才行)
+     *
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] arr, int i, int j) {
+        /*arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];*/
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    /**
+     * 随机生成随机长度，随机值的数组
+     * <p>
+     * 对数器
+     * <p>
+     * 1，你想要测的方法a
+     * 2，实现复杂度不好但是容易实现的方法b
+     * 3，实现一个随机样本产生器
+     * 4，把方法a和方法b跑相同的随机样本，看看得到的结果是否一样
+     * 5，如果有一个随机样本使得比对结果不一致，打印样本进行人工干预，改对方法a和方法b
+     * 6，当样本数量很多时比对测试依然正确，可以确定方法a已经正确。
+     *
+     * @param maxSize
+     * @param maxValue
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) ||
+                (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
