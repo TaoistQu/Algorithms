9eb703b19e1ca66831283061c3a7b3f7acfd8103
TaoistQu <qulei_20180331@163.com>
2021-01-28 23:57:49 +0800

    Add:增加双向链表实现双端队列实现的队列

---
+++ src/main/java/cn/dataStructures/queue/MyQueue.java
@@ -0,0 +1,34 @@
+package cn.dataStructures.queue;
+
+import cn.dataStructures.util.DoubleEndsQueue;
+
+/**
+ * 队列
+ *
+ * @author TaoistQu
+ * @version 1.0
+ * @date 2021/1/28 22:24
+ */
+public class MyQueue<T> {
+    private DoubleEndsQueue<T> queue;
+
+    public MyQueue() {
+        queue = new DoubleEndsQueue<>();
+    }
+
+    public void push(T value) {
+        queue.addFromHead(value);
+    }
+
+    public T poll() {
+        return queue.popFromBottom();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
