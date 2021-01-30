bc0ecc1863da29a24ea092d390669471c63e305c
TaoistQu <qulei_20180331@163.com>
2021-01-30 23:13:30 +0800

    Add:用获取数组最大值测试递归调用

---
+++ src/main/java/cn/algorithms/recursion/GetMax.java
@@ -0,0 +1,34 @@
+package cn.algorithms.recursion;
+
+import cn.algorithms.util.ArrayUtil;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+/**
+ * 测试递归
+ *
+ * @author TaoistQu
+ * @version 1.0
+ * @date 2021/1/30 22:46
+ */
+public class GetMax {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtil.generateRandomArray(30, 100);
+        int max = getMax(arr);
+        System.out.println(max);
+    }
+
+    public static int getMax(int[] arr) {
+        return process(arr, 0, arr.length - 1);
+    }
+
+    public static int process(int[] arr, int L, int R) {
+        if (L == R) {
+            return arr[L];
+        }
+        int mid = L + ((R - L) >> 1);
+        int leftMax = process(arr, L, mid);
+        int rightMax = process(arr, mid + 1, R);
+        return Math.max(leftMax, rightMax);
+    }
+}
