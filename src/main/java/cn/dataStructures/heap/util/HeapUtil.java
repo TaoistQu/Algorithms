c76a4177c45ae1533d01dfb368ee75e8f9832b51
TaoistQu <qulei_20180331@163.com>
2021-05-16 23:18:21 +0800

    Update:优化乘除法，用二进制移位操作

--- src/main/java/cn/dataStructures/heap/util/HeapUtil.java
+++ src/main/java/cn/dataStructures/heap/util/HeapUtil.java
@@ -17,7 +17,7 @@ public class HeapUtil {
     public static void heapInsert(int[] arr, int index) {
         while (arr[index] > arr[((index - 1) / 2)]) {
             ArrayUtil.swap(arr, index, (index - 1) / 2);
-            index = (index - 1) / 2;
+            index = (index - 1) >>> 1;
         }
     }
 
@@ -27,7 +27,7 @@ public class HeapUtil {
      * @param heapSize 堆的大小
      */
     public static void heapify(int[] arr, int index, int heapSize) {
-        int left = index * 2 + 1;
+        int left = (index << 1) + 1;
         while (left < heapSize) {
             int largest = (left + 1) < heapSize && arr[left + 1] > arr[left] ? (left + 1) : left;
             largest = arr[largest] > arr[index] ? largest : index;
