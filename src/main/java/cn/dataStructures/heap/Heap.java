c80f2d59f816c641e0b3410386a0578168bbb136
TaoistQu <qulei_20180331@163.com>
2021-05-16 17:46:41 +0800

    Update:增加堆数据结构的一些说明

--- src/main/java/cn/dataStructures/heap/Heap.java
+++ src/main/java/cn/dataStructures/heap/Heap.java
@@ -6,7 +6,13 @@ import cn.dataStructures.heap.util.HeapUtil;
 /**
  * Created with IntelliJ IDEA.
  *
- * @Description: 大根堆
+ * @Description: 大根堆 即为完全二叉树 任意一颗子树的最大值在树根上
+ * <p>
+ * i位置的节点的
+ * 左子节点索引为：2*i+1
+ * 右子节点索引坐标为：2*i+2
+ * 父节点坐标为： (i-1)/2
+ * </p>
  * @Auther: TaoistQu
  * @Date: 2021/05/15/19:18
  */
