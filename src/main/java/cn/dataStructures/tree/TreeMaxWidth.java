1169e1e1e1ff9cf4d81c42d804f7e6cc26fff943
TaoistQu <qulei_20180331@163.com>
2021-06-19 11:53:07 +0800

    Add:获取二叉树的最大宽度

---
+++ src/main/java/cn/dataStructures/tree/TreeMaxWidth.java
@@ -0,0 +1,102 @@
+package cn.dataStructures.tree;
+
+import cn.dataStructures.tree.util.TreeNode;
+import cn.dataStructures.tree.util.TreeUtil;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:
+ * @Auther: TaoistQu
+ * @Date: 2021/06/19/0:52
+ */
+public class TreeMaxWidth {
+    /**
+     * 使用容器做层数和个数统计
+     *
+     * @param head
+     * @return
+     */
+    public static int maxWidthUseMap(TreeNode head) {
+        if (head == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(head);
+        HashMap<TreeNode, Integer> levelMap = new HashMap<>();
+        levelMap.put(head, 1);
+        int curLevel = 1;   // 当前你正在统计哪一层的宽度
+        int curLevelNodes = 0;  // 当前层curLevel层，宽度目前是多少
+        int max = 0;
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            int curNodeLevel = levelMap.get(cur);
+            if (cur.left != null) {
+                levelMap.put(cur.left, curNodeLevel + 1);
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                levelMap.put(cur.right, curNodeLevel + 1);
+                queue.add(cur.right);
+            }
+            if (curNodeLevel == curLevel) {
+                curLevelNodes++;
+            } else {
+                max = Math.max(max, curLevelNodes);
+                curLevel++;
+                curLevelNodes = 1;
+            }
+        }
+        max = Math.max(max, curLevelNodes);
+        return max;
+    }
+
+    /**
+     * 不使用容器实现最大宽度获取
+     *
+     * @param head
+     * @return
+     */
+    public static int maxWidthNoMap(TreeNode head) {
+        if (head == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(head);
+        TreeNode curEnd = head;
+        TreeNode nextEnd = null;
+        int max = 0;
+        int curLevelNodes = 0;
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (cur.left != null) {
+                queue.add(cur.left);
+                nextEnd = cur.left;
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+                nextEnd = cur.right;
+            }
+            curLevelNodes++;
+            if (cur == curEnd) {
+                max = Math.max(max, curLevelNodes);
+                curEnd = nextEnd;
+                curLevelNodes = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 10;
+        int maxValue = 100;
+        int testTimes = 1000000000;
+        for (int i = 0; i < testTimes; i++) {
+            TreeNode head = TreeUtil.generateRandomBST(maxLevel, maxValue);
+            if (maxWidthNoMap(head) != maxWidthUseMap(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+}
