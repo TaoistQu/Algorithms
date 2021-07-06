03fd8d89b93c269a8682392eaa24ebcbcb192a3d
TaoistQu <qulei_20180331@163.com>
2021-07-07 00:56:33 +0800

    Add:贪心策略做项目

---
+++ src/main/java/cn/algorithms/greedy/IPO.java
@@ -0,0 +1,63 @@
+package cn.algorithms.greedy;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:输入: 正数数组costs、正数数组profits、正数K、正数M
+ * costs[i]表示i号项目的花费
+ * profits[i]表示i号项目在扣除花费之后还能挣到的钱(利润)
+ * K表示你只能串行的最多做k个项目
+ * M表示你初始的资金
+ * 说明: 每做完一个项目，马上获得的收益，可以支持你去做下一个项目。不能并行的做项目。
+ * 输出：你最后获得的最大钱数
+ * @Auther: TaoistQu
+ * @Date: 2021/07/07/0:45
+ */
+public class IPO {
+    public static class Program {
+        public int p;
+        public int c;
+
+        public Program(int p, int c) {
+            this.p = p;
+            this.c = c;
+        }
+    }
+
+    public static class MinCostComparator implements Comparator<Program> {
+
+        @Override
+        public int compare(Program o1, Program o2) {
+            return o1.c - o2.c;
+        }
+    }
+
+    public static class MaxProfitComparator implements Comparator<Program> {
+
+        @Override
+        public int compare(Program o1, Program o2) {
+            return o2.p - o1.p;
+        }
+    }
+
+    public static int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        PriorityQueue<Program> minCostQ = new PriorityQueue<>(new MinCostComparator());
+        PriorityQueue<Program> maxProfitQ = new PriorityQueue<>(new MaxProfitComparator());
+        for (int i = 0; i < Profits.length; i++) {
+            minCostQ.add(new Program(Profits[i], Capital[i]));
+        }
+        for (int i = 0; i < k; i++) {
+            while (!maxProfitQ.isEmpty() && minCostQ.peek().c <= W) {
+                maxProfitQ.add(minCostQ.poll());
+            }
+            if (maxProfitQ.isEmpty()) {
+                return W;
+            }
+            W += maxProfitQ.poll().p;
+        }
+        return W;
+    }
+}
