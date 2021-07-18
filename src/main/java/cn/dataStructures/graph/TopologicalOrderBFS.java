7ceedcc4fa899adf477dccdd31b55b31f55e3405
TaoistQu <qulei_20180331@163.com>
2021-07-18 23:29:40 +0800

    Add:用到达点数完成拓扑序

---
+++ src/main/java/cn/dataStructures/graph/TopologicalOrderBFS.java
@@ -0,0 +1,56 @@
+package cn.dataStructures.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: 用到达点数完成拓扑序
+ * @Auther: TaoistQu
+ * @Date: 2021/07/18/23:15
+ */
+public class TopologicalOrderBFS {
+    public static class DirectedGraphNode {
+        public int label;
+        public ArrayList<DirectedGraphNode> neighbors;
+
+        public DirectedGraphNode(int x) {
+            label = x;
+            neighbors = new ArrayList<>();
+        }
+    }
+
+    public static ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        HashMap<DirectedGraphNode, Integer> indegreeMap = new HashMap<>();
+        for (DirectedGraphNode cur : graph) {
+            indegreeMap.put(cur, 0);
+        }
+        for (DirectedGraphNode cur : graph) {
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) + 1);
+            }
+        }
+        Queue<DirectedGraphNode> zeroQueue = new LinkedList<>();
+        for (DirectedGraphNode cur : indegreeMap.keySet()) {
+            if (indegreeMap.get(cur) == 0) {
+                zeroQueue.add(cur);
+            }
+        }
+        ArrayList<DirectedGraphNode> ans = new ArrayList<>();
+        while (!zeroQueue.isEmpty()) {
+            DirectedGraphNode cur = zeroQueue.poll();
+            ans.add(cur);
+            for (DirectedGraphNode next : cur.neighbors) {
+                indegreeMap.put(next, indegreeMap.get(next) - 1);
+                if (indegreeMap.get(next) == 0) {
+                    zeroQueue.offer(next);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
