a8d558ee315fbe91a53886ab9b172b57ff6b0e3b
TaoistQu <qulei_20180331@163.com>
2021-07-19 00:12:08 +0800

    Add:用最大深度的方式求拓扑序

--- src/main/java/cn/dataStructures/graph/TopologicalOrderDFS2.java
+++ src/main/java/cn/dataStructures/graph/TopologicalOrderDFS2.java
@@ -9,21 +9,21 @@ import java.util.HashMap;
 /**
  * Created with IntelliJ IDEA.
  *
- * @Description:用点次排拓扑序，若X的最大点次比Y的大，则X在Y前面
+ * @Description: 最大深度，如果X的最大深度大与Y的最大深度大x就在Y前
  * @Auther: TaoistQu
- * @Date: 2021/07/18/23:30
+ * @Date: 2021/07/18/23:42
  */
-public class TopologicalOrderDFS1 {
+public class TopologicalOrderDFS2 {
     /**
-     * 当前点的点次
+     * 当前点的深度
      */
     public static class Record {
         public DirectedGraphNode node;
-        public long nodes;
+        public int deep;
 
-        public Record(DirectedGraphNode n, long o) {
+        public Record(DirectedGraphNode n, int d) {
             node = n;
-            nodes = o;
+            deep = d;
         }
     }
 
@@ -31,14 +31,10 @@ public class TopologicalOrderDFS1 {
 
         @Override
         public int compare(Record o1, Record o2) {
-            return o1.nodes == o2.nodes ? 0 : (o1.nodes > o2.nodes ? -1 : 1);
+            return o2.deep - o1.deep;
         }
     }
 
-    /**
-     * @param graph
-     * @return
-     */
     public static ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
         HashMap<DirectedGraphNode, Record> order = new HashMap<>();
         for (DirectedGraphNode cur : graph) {
@@ -60,11 +56,11 @@ public class TopologicalOrderDFS1 {
         if (order.containsKey(cur)) {
             return order.get(cur);
         }
-        long nodes = 0;
+        int follow = 0;
         for (DirectedGraphNode next : cur.neighbors) {
-            nodes += f(next, order).nodes;
+            follow = Math.max(follow, f(next, order).deep);
         }
-        Record ans = new Record(cur, nodes + 1);
+        Record ans = new Record(cur, follow + 1);
         order.put(cur, ans);
         return ans;
     }
