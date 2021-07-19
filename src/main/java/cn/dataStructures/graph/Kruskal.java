d006fd5dab877fe5330b8525d2119c013fa134f8
TaoistQu <qulei_20180331@163.com>
2021-07-19 23:59:07 +0800

    Add:图的最小生成树

---
+++ src/main/java/cn/dataStructures/graph/Kruskal.java
@@ -0,0 +1,101 @@
+package cn.dataStructures.graph;
+
+import cn.dataStructures.graph.util.Edge;
+import cn.dataStructures.graph.util.Graph;
+import cn.dataStructures.graph.util.Node;
+import com.sun.org.apache.bcel.internal.generic.SIPUSH;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: 求图的最小树
+ * @Auther: TaoistQu
+ * @Date: 2021/07/19/0:16
+ */
+public class Kruskal {
+
+    public static class UnionFind {
+        private HashMap<Node, Node> fatherMap;
+        private HashMap<Node, Integer> sizeMap;
+
+        public UnionFind() {
+            fatherMap = new HashMap<>();
+            sizeMap = new HashMap<>();
+        }
+
+        public void makeSets(Collection<Node> nodes) {
+            fatherMap.clear();
+            sizeMap.clear();
+            for (Node node : nodes) {
+                fatherMap.put(node, node);
+                sizeMap.put(node, 1);
+            }
+        }
+
+        private Node findFather(Node n) {
+            Stack<Node> path = new Stack<>();
+            while (n != fatherMap.get(n)) {
+                path.push(n);
+                n = fatherMap.get(n);
+            }
+            while (!path.isEmpty()) {
+                fatherMap.put(path.pop(), n);
+            }
+            return n;
+        }
+
+        public boolean isSameSet(Node a, Node b) {
+            return findFather(a) == findFather(b);
+        }
+
+        public void union(Node a, Node b) {
+            if (a == null || b == null) {
+                return;
+            }
+            Node aDai = findFather(a);
+            Node bDai = findFather(b);
+            if (aDai != bDai) {
+                int aSetSize = sizeMap.get(aDai);
+                int bSetSize = sizeMap.get(bDai);
+                if (aSetSize <= bSetSize) {
+                    fatherMap.put(bDai, aDai);
+                    sizeMap.put(bDai, aSetSize + bSetSize);
+                    sizeMap.remove(aDai);
+                } else {
+                    fatherMap.put(aDai, bDai);
+                    sizeMap.put(aDai, aSetSize + bSetSize);
+                    sizeMap.remove(bDai);
+                }
+            }
+        }
+    }
+
+    public static class EdgeComparator implements Comparator<Edge> {
+        @Override
+        public int compare(Edge o1, Edge o2) {
+            return o1.weight - o2.weight;
+        }
+    }
+
+    public static Set<Edge> kruskalMST(Graph graaph) {
+        UnionFind unionFind = new UnionFind();
+        unionFind.makeSets(graaph.nodes.values());
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new EdgeComparator());
+        for (Edge edge : graaph.edges) {
+            priorityQueue.add(edge);
+        }
+        Set<Edge> result = new HashSet<>();
+        while (!priorityQueue.isEmpty()) {
+            Edge edge = priorityQueue.poll();
+            if (!unionFind.isSameSet(edge.from, edge.to)) {
+                result.add(edge);
+                unionFind.union(edge.from, edge.to);
+            }
+        }
+        return result;
+    }
+
+
+}
